614f60f092e701657aa9b10a2ae745ade05ddf6f
shaiqkar <shaiqkar@gmail.com>
2020-07-14 17:12:11 +0530

    Added Sidebar

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,22 @@ function toggleTextVisibility() {
     hiddenText.removeAttribute('hidden');
     toggleBtn.setAttribute('hidden', '')
 }
+
+
+let sidenav = document.getElementById('sidenav-mobile')
+
+document.getElementById('sidebar-btn').onclick = () => { 
+    sidenav.classList.add('mobile-sidebar-active')
+}
+document.querySelector(".app").addEventListener("click", () => {
+    sidenav.classList.remove('mobile-sidebar-active')
+})
+sidenav.addEventListener('touchmove', (event) => {
+    event.preventDefault();
+}, false);
+
+let dropNav = document.getElementById('drop-nav-btn')
+dropNav.onclick = () => {
+    dropNav.classList.toggle("drop-active-btn")
+    document.getElementById('drop-nav').classList.toggle("drop-active")
+}
